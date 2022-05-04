5d1a1181bf5d441025df7634c43de389fc7368bc
bun133 <bun133.work@gmail.com>
2022-05-04 15:56:23 +0900

    Add Sample

---
+++ sample/src/main/java/com/github/bun133/tinked/sampleplugin/Sampleplugin.kt
@@ -0,0 +1,13 @@
+package com.github.bun133.tinked.sampleplugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Sampleplugin : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}

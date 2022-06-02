8ace43c202af31ee6b135588193ef3784a296d89
bun133 <bun133.work@gmail.com>
2022-06-02 20:21:51 +0900

    Add:RunnableTaskContext

--- src/main/java/com/github/bun133/tinked/RunnableTask.kt
+++ src/main/java/com/github/bun133/tinked/RunnableTask.kt
@@ -5,18 +5,36 @@ package com.github.bun133.tinked
  */
 open class RunnableTask<I, R>(val r: RunnableTaskContext<I, R>.(I) -> R) : Task<I, R>() {
     override fun runnable(input: I): R {
-        return RunnableTaskContext(this).r(input)
+        throw UnsupportedOperationException("runnable task is not supported")
     }
+
+    override fun run(input: I) {
+        val ctx = RunnableTaskContext(this, input)
+        ctx.run()
+    }
+
+    internal fun getNextNode() = nextNode
 }
 
 /**
  * Runnable TaskのContext
  */
-class RunnableTaskContext<I, R>(private val task: RunnableTask<I, R>) {
+class RunnableTaskContext<I, R>(private val task: RunnableTask<I, R>, private val input: I) {
+
+    private val toApply = mutableListOf<Task<R, *>>()
+
     /**
      * このタスクの後に実行するタスクを設定する
+     * ただし、返り値は無視される
+     * このメソッドを介して追加されたタスクはこのタスク終了後、逐次実行される
      */
-    fun <T : Any> join(otherTask: Task<R, T>) {
-        task.then(otherTask)
+    fun apply(otherTask: Task<R, *>) {
+        toApply.add(otherTask)
+    }
+
+    internal fun run() {
+        val r = task.r(this, input)
+        toApply.forEach { it.run(r) }
+        task.getNextNode()?.run(r)
     }
 }

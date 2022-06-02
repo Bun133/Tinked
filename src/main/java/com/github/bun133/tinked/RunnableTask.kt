30d78913827157851ae76eb0ffe1d731eb67a6b7
bun133 <bun133.work@gmail.com>
2022-06-02 21:12:59 +0900

    Update:RunnableTaskContext

--- src/main/java/com/github/bun133/tinked/RunnableTask.kt
+++ src/main/java/com/github/bun133/tinked/RunnableTask.kt
@@ -21,20 +21,20 @@ open class RunnableTask<I, R>(val r: RunnableTaskContext<I, R>.(I) -> R) : Task<
  */
 class RunnableTaskContext<I, R>(private val task: RunnableTask<I, R>, private val input: I) {
 
-    private val toApply = mutableListOf<Task<R, Any>>()
+    private val toApply = mutableListOf<Task<R, Any?>>()
 
     /**
      * このタスクの後に実行するタスクを設定する
      * ただし、返り値は無視される
      * このメソッドを介して追加されたタスクはこのタスク終了後、パラレル実行される
      */
-    fun apply(otherTask: Task<R, Any>) {
+    fun apply(otherTask: Task<R, Any?>) {
         toApply.add(otherTask)
     }
 
     internal fun run() {
         val r = task.r(this, input)
-        Task.all(*toApply.toTypedArray()).then(RunnableTask<List<Any>, Unit> {
+        Task.all(*toApply.toTypedArray()).then(RunnableTask<List<Any?>, Unit> {
             this@RunnableTaskContext.task.getNextNode()?.run(r)
         })
     }

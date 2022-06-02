e04be87479e4e03be128b1e1b4463718992d07e2
bun133 <bun133.work@gmail.com>
2022-06-02 19:58:38 +0900

    Add:RunnableTaskContext

--- src/main/java/com/github/bun133/tinked/RunnableTask.kt
+++ src/main/java/com/github/bun133/tinked/RunnableTask.kt
@@ -3,8 +3,20 @@ package com.github.bun133.tinked
 /**
  * Runnable Task
  */
-open class RunnableTask<I, R>(val r: (I) -> R) : Task<I, R>() {
+open class RunnableTask<I, R>(val r: RunnableTaskContext<I, R>.(I) -> R) : Task<I, R>() {
     override fun runnable(input: I): R {
-        return r(input)
+        return RunnableTaskContext(this).r(input)
+    }
+}
+
+/**
+ * Runnable TaskのContext
+ */
+class RunnableTaskContext<I, R>(private val task: RunnableTask<I, R>) {
+    /**
+     * このタスクの後に実行するタスクを設定する
+     */
+    fun <T : Any> join(otherTask: Task<R, T>) {
+        task.then(otherTask)
     }
 }
